7532855e7e95576226ff2b2194fe56d6f319ab58
kavishkaHerath <hmravindu@gmail.com>
2025-07-24 01:54:41 +0530

    Check if the token is valid

--- src/main/java/com/spn/security/config/JwtService.java
+++ src/main/java/com/spn/security/config/JwtService.java
@@ -26,6 +26,7 @@ public class JwtService {
         final  Claims claims = extractClaims(token);
         return claimsResolver.apply(claims);
     }
+
     public String generateToken(UserDetails userDetails) {
         return generateToken(new HashMap<>(), userDetails);
     }
@@ -44,6 +45,18 @@ public class JwtService {
                 .compact();
     }
 
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
 
     private Claims extractClaims(String token) {
         return Jwts
