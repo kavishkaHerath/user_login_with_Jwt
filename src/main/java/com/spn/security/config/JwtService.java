47ccdccf9b9cdb7a280466d9a55e472d93632ed9
kavishkaHerath <hmravindu@gmail.com>
2025-07-23 13:26:45 +0530

    Extract the username from the token

--- src/main/java/com/spn/security/config/JwtService.java
+++ src/main/java/com/spn/security/config/JwtService.java
@@ -14,8 +14,9 @@ public class JwtService {
     private static final String SECRET_KEY = "a5521aef444939a15c67b8fdffc0f340011fb562255f267a73786de567ed1027";
 
     public String extractUsername(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
     }
+
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final  Claims claims = extractClaims(token);
         return claimsResolver.apply(claims);
