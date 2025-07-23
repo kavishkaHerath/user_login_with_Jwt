0e3aba08119b9de2b88372f792ab444952e606ad
kavishkaHerath <hmravindu@gmail.com>
2025-07-23 13:23:47 +0530

    Implement the getSignInKey method

--- src/main/java/com/spn/security/config/JwtService.java
+++ src/main/java/com/spn/security/config/JwtService.java
@@ -1,11 +1,35 @@
 package com.spn.security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
+    private static final String SECRET_KEY = "a5521aef444939a15c67b8fdffc0f340011fb562255f267a73786de567ed1027";
 
     public String extractUsername(String token) {
         return null;
+    }
+
+
+
+    private Claims extractClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
